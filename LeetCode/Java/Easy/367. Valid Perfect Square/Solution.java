8f1aa939747e714080d6f397bd0765bcc9864624
sathvika-ch <chavanaboinasathvika@gmail.com>
2026-08-19 10:27:46 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/367. Valid Perfect Square/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+     int start = 1;
+     int end = num;
+    while(start<=end){
+       int mid = start + ((end-start)/2);
+        if (mid * mid == num)
+            return true ;
+        else if(mid * mid > num)
+            end = mid - 1;
+        else
+            start = mid + 1;
+    }    
+     return false ;      
+    }
+    
+}
+    
