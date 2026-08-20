86acb4b2c7fd1fb53499dea220e1d1032316342a
sathvika-ch <chavanaboinasathvika@gmail.com>
2026-08-20 21:56:14 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/73. Set Matrix Zeroes/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] column = new boolean [n];
+        for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    row[i]=true;
+                    column[j]=true;
+
+                }
+            }
+        }
+        
+        for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i] || column[j])
+                matrix[i][j] = 0;
+            }
+        }
+            }
+        
+        
+    
+}
