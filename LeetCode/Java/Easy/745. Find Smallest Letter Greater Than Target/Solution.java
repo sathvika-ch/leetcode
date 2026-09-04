6230b19e6fe970a380b97ef39df5379c68a586c2
sathvika-ch <chavanaboinasathvika@gmail.com>
2026-09-04 10:33:25 +0530

    Add Easy Java Solution for 745. Find Smallest Letter Greater Than Target from LeetCode

--- LeetCode/Java/Easy/745. Find Smallest Letter Greater Than Target/Solution.java
+++ LeetCode/Java/Easy/745. Find Smallest Letter Greater Than Target/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        int len =letters.length-1;
+        int len =letters.length;
         for(int i=0;i<len;i++){
             if(letters[i]>target){
                 return letters[i];
